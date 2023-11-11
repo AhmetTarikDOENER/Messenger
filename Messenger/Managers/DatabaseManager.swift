a84bd67360e8980024913c80fda6e0c5d8ea136a
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-11-11 19:32:34 +0300

    Fixed bugs that occurs account registration issue.

--- Messenger/Managers/DatabaseManager.swift
+++ Messenger/Managers/DatabaseManager.swift
@@ -21,14 +21,17 @@ extension DatabaseManager {
     
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
     }
     
     public func isUserExist(with email: String, completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of: .value) {
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value) {
             snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
@@ -45,4 +48,10 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
 }
